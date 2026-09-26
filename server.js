73c388222eb81e2927f83d62078b663f3abf5368
Danilo Lazarevic <116965049+dacostumble@users.noreply.github.com>
2026-09-26 16:14:59 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -309,11 +309,11 @@ server.get('/admin_lista_jela' , async (zahtev, odg) => {
 //front end proverava dostupnost:
 server.get('/proveri_dostupnost' , async(zahtev, odg) => {
     try{
-        let lista_dostupnih=await jela.find(
-            {dostupno: true}
+        let lista_nedostupnih=await jela.find(
+            {dostupno: false}
         );
 
-        odg.send(lista_dostupnih);
+        odg.send(lista_nedostupnih);
     }
 
     catch(error){
